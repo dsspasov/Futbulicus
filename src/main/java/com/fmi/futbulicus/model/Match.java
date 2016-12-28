735d3dcf8563fa105a8a66191e88f1c56f9cba46
Simeon Markov <Simeon Markov@Simeon_Markov>
2016-12-28 23:46:32 +0200

    added some getters/setters and toString

--- src/main/java/com/fmi/futbulicus/model/Match.java
+++ src/main/java/com/fmi/futbulicus/model/Match.java
@@ -25,4 +25,43 @@ public class Match {
 	
 	@OneToOne
 	private Team guest;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+
+	public Team getHost() {
+		return host;
+	}
+
+	public void setHost(Team host) {
+		this.host = host;
+	}
+
+	public Team getGuest() {
+		return guest;
+	}
+
+	public void setGuest(Team guest) {
+		this.guest = guest;
+	}
+
+	@Override
+	public String toString() {
+		return "Match [id=" + id + ", result=" + result + ", host=" + host + ", guest=" + guest + "]";
+	}
+	
+	
 }
