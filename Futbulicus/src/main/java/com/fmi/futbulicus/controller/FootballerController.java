3613d0e13ce907164c0d15fbb0d7f5c5cea974ab
SimeonMarkov <simeon.markov1994@gmail.com>
2017-01-07 20:53:59 +0200

    commented footballer controller

--- Futbulicus/src/main/java/com/fmi/futbulicus/controller/FootballerController.java
+++ Futbulicus/src/main/java/com/fmi/futbulicus/controller/FootballerController.java
@@ -1,33 +1,33 @@
 package com.fmi.futbulicus.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.fmi.futbulicus.model.Footballer;
-import com.fmi.futbulicus.repository.FootballerRepository;
+//import com.fmi.futbulicus.model.Footballer;
+//import com.fmi.futbulicus.repository.FootballerRepository;
 
 @Controller
 public class FootballerController {
 
-	@Autowired
-	private FootballerRepository footballerRepository;
-	
-	@RequestMapping(value = "/footballers", method = RequestMethod.GET)
-	public String getFootballer(Model model) {
-		Iterable<Footballer> footballers = footballerRepository.findAll();
-		model.addAttribute("footballers", footballers);
-		return "/footballers";
-	}
-	
-	@RequestMapping(value = "/footballers/footballer/{id}", method = RequestMethod.GET)
-	public String getFootballer(@PathVariable("id") Long id, Model model) {
-		Footballer footballer = footballerRepository.findOne(id);
-		model.addAttribute("footballer", footballer);
-		return "/footballer";
-	}
+//	@Autowired
+//	private FootballerRepository footballerRepository;
+//	
+//	@RequestMapping(value = "/footballers", method = RequestMethod.GET)
+//	public String getFootballer(Model model) {
+//		Iterable<Footballer> footballers = footballerRepository.findAll();
+//		model.addAttribute("footballers", footballers);
+//		return "/footballers";
+//	}
+//	
+//	@RequestMapping(value = "/footballers/footballer/{id}", method = RequestMethod.GET)
+//	public String getFootballer(@PathVariable("id") Long id, Model model) {
+//		Footballer footballer = footballerRepository.findOne(id);
+//		model.addAttribute("footballer", footballer);
+//		return "/footballer";
+//	}
 	
 }
