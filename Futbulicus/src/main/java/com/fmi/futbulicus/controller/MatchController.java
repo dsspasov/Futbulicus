c5c2c9cf16ad08718eb8414e253b38dffc49e427
Simeon Markov <Simeon Markov@Simeon_Markov>
2017-01-07 20:52:12 +0200

    commented all the users logic

--- Futbulicus/src/main/java/com/fmi/futbulicus/controller/MatchController.java
+++ Futbulicus/src/main/java/com/fmi/futbulicus/controller/MatchController.java
@@ -1,31 +1,31 @@
 package com.fmi.futbulicus.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.fmi.futbulicus.model.Match;
-import com.fmi.futbulicus.repository.MatchRepository;
+//import com.fmi.futbulicus.model.Match;
+//import com.fmi.futbulicus.repository.MatchRepository;
 
 @Controller
 public class MatchController {
 	
-	@Autowired
-	private MatchRepository matchRepository;
-	@RequestMapping(value = "/matches", method = RequestMethod.GET)
-	public String getMatches(Model model) {
-		Iterable<Match> matches = matchRepository.findAll();
-		model.addAttribute("matches", matches);
-		return "/matches";
-	}
-	
-	@RequestMapping(value = "/matches/match/{id}", method = RequestMethod.GET)
-	public String getMatch(@PathVariable("id") Long id, Model model) {
-		Match match = matchRepository.findOne(id);
-		model.addAttribute("match", match);
-		return "/match";
-	}
+//	@Autowired
+//	private MatchRepository matchRepository;
+//	@RequestMapping(value = "/matches", method = RequestMethod.GET)
+//	public String getMatches(Model model) {
+//		Iterable<Match> matches = matchRepository.findAll();
+//		model.addAttribute("matches", matches);
+//		return "/matches";
+//	}
+//	
+//	@RequestMapping(value = "/matches/match/{id}", method = RequestMethod.GET)
+//	public String getMatch(@PathVariable("id") Long id, Model model) {
+//		Match match = matchRepository.findOne(id);
+//		model.addAttribute("match", match);
+//		return "/match";
+//	}
 }
--- Futbulicus/src/main/java/com/fmi/futbulicus/controller/TeamController.java
+++ Futbulicus/src/main/java/com/fmi/futbulicus/controller/TeamController.java
@@ -7,15 +7,15 @@ import java.util.HashMap;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.fmi.futbulicus.model.Team;
-import com.fmi.futbulicus.repository.TeamRepository;
+//import com.fmi.futbulicus.model.Team;
+//import com.fmi.futbulicus.repository.TeamRepository;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
@@ -25,15 +25,15 @@ public class TeamController {
 
 	private static final String PLAYERS_TEAM_URL = "https://euadmin4.backstage.spotme.com/api/v1/eid/cbe9ff2c721f63e6347ca3f66ce21177/nodehandlers/soccer/stats?";
 	
-	@Autowired
-	private TeamRepository teamRepository;
-	
-	@RequestMapping(value = "/teams", method = RequestMethod.GET)
-	public String getTeams(Model model) {
-		Iterable<Team> teams = teamRepository.findAll();
-		model.addAttribute("teams", teams);
-		return "/teams";
-	}
+//	@Autowired
+//	private TeamRepository teamRepository;
+//	
+//	@RequestMapping(value = "/teams", method = RequestMethod.GET)
+//	public String getTeams(Model model) {
+//		Iterable<Team> teams = teamRepository.findAll();
+//		model.addAttribute("teams", teams);
+//		return "/teams";
+//	}
 	
 	@RequestMapping(value = "/teams/team/{id}")
 	public String getTeam(@PathVariable("id") String id, HttpServletRequest request, Model model) throws IOException {
